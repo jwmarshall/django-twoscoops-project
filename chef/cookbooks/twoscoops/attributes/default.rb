eab8132a3b4db7b83fe371ef6347e0b416993797
Jonathon W. Marshall <jonathon@bnotions.com>
2013-08-17 17:44:08 -0400

    chef cookbooks

---
+++ chef/cookbooks/twoscoops/attributes/default.rb
@@ -0,0 +1,7 @@
+# Default attributes
+
+default["twoscoops"]["project_name"] = nil
+
+default["twoscoops"]["superuser"]["username"] = "vagrant"
+default["twoscoops"]["superuser"]["password_hash"] = "pbkdf2_sha256$10000$NoIByEhX0v78$UgkCwmSHBNYiFPD0zCkZ9x+S7z5tlRysHv/L68OJdxc="
+
